a09475f0bdec50d005ad1ab6893b3f5c2bea521e
lls <1351200573@qq.com>
2016-12-27 09:42:18 +0800

    2

--- src/com/lls/helloworld/HelloWorld.java
+++ src/com/lls/helloworld/HelloWorld.java
@@ -2,7 +2,7 @@ package com.lls.helloworld;
 
 public class HelloWorld {
 	public static void main(String []args){
-		System.out.println("Hello World");
+		System.out.println("Hello World2");
 	}
 
 
